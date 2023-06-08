d1c0bbbb652dbc5bc8c842cf8ee878750211611d
Kayode Lawal <63661278+lawalkeyd@users.noreply.github.com>
2023-06-08 16:07:52 +0000

    Initial commit

---
+++ event_stream/event_stream/doctype/event_producers_last_update/event_producers_last_update.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Kayode Lawal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Event Producers Last Update", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ event_stream/event_stream/doctype/event_sync_logs/event_sync_logs.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2023, Kayode Lawal and contributors
+// For license information, please see license.txt
+
+
+frappe.ui.form.on("Event Sync Logs", {
+	refresh: function (frm) {
+		if (frm.doc.status == "Failed") {
+			frm.add_custom_button(__("Resync"), function () {
+				frappe.call({
+					method: "event_streaming.event_streaming.doctype.event_producers.event_producers.resync",
+					args: {
+						update: frm.doc,
+					},
+					callback: function (r) {
+						if (r.message) {
+							frappe.msgprint(r.message);
+							frm.set_value("status", r.message);
+							frm.save();
+						}
+					},
+				});
+			});
+		}
+	},
+});
